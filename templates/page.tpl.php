e5328fe3d3c003d67a3d00ad528ad3870381ebdc
Paolo Agostinelli <dodazzi@gmail.com>
2011-04-21 16:35:57 -0400

    Update style

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -89,9 +89,9 @@
     <?php print $messages; ?>
     <?php print render($tabs); ?>
 
-    <!-- <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?> -->
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
         
     <?php print render($page['content']); ?>
     <?php print $feed_icons; ?>
